3388c059c9f25f827070d68ddb164bb1c9a79856
Jump3rr <66004248+Jump3rr@users.noreply.github.com>
2021-10-18 22:15:51 +0200

    Task 1 completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import "./styles.css";
+import { UserDetailsView } from "./components/UserDetailsView";
+import { Provider } from "react-redux";
+import store from "./store/store";
 
 export default function App() {
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <UserDetailsView />
+      </div>
+    </Provider>
   );
 }
